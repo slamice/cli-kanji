15793dd90077defbb710946c87b0bc3fa2978b03
issam.zeibak <issam.zeibak@gengo.com>
2014-04-21 20:16:22 +0900

    + grabbing for levels

--- __main__.py
+++ __main__.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import yaml
+import random
 
+KANJI_YAML = 'kanji.yaml'
+IGNORE_LIST = 'ignore_list.yaml'
+
+def random_level():
+    return 'N' + str(random.randint(1, 5))
 
 def get_random_kanji(level):
-    stream = open("kanji.yaml", "r")
-    docs = yaml.load_all(stream)
-    for doc in docs:
-        for k,v in doc.items():
-            if k == 'kanji':
-                print k, "->", v.encode('utf-8')
-        print "\n",
+    f = open(KANJI_YAML, 'r')
+    dataMap = yaml.safe_load(f)
+    f.close()
+
+    if level == '':
+        level = random_level()
+
+    kanji = dataMap[level]
+    print random.choice(kanji)
 
 
+#def remove_kanji(kanji):
+#    stream = open(KANJI_YAML, 'w')
+
+#def reset_ignore_list():
+
+# Get input from user
 if __name__ == '__main__':
-    get_random_kanji('N5')
+    get_random_kanji('')
 
-# specify level
-# Default randomly fetches kanji
+# -l specify level
+# -i adds kanji to ignore list
+# Default fetches random kanji
 # optional: specify Kanji
