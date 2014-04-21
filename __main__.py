f9d31e337e888ab2cdb98fd62f5af320290a46cd
slamice <slamice@gmail.com>
2014-04-22 00:29:18 +0900

    fixed it so kanji now displays

--- __main__.py
+++ __main__.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 import yaml
 import random
+import argparse
+import pprint
+
 
 KANJI_YAML = 'kanji.yaml'
 IGNORE_LIST = 'ignore_list.yaml'
@@ -18,19 +21,47 @@ def get_random_kanji(level):
         level = random_level()
 
     kanji = dataMap[level]
-    print random.choice(kanji)
+    selection = random.choice(kanji)
+    char = selection['kanji']
+
+    print 'What does '+ char + ' mean?'
+    var = raw_input()
+
+    if iequal(var, selection['Meaning']):
+        print '\n-==*== Yes! You got it! ==*==-\n'
+        print_kanji(selection)
+    else:
+        print '\nNo, try another one :( \n'
+        print_kanji(selection)
+
+# Equals (ignore case)
+def iequal(a, b):
+    try:
+       return a.upper() == b.upper()
+    except AttributeError:
+       return a == b
 
 
-#def remove_kanji(kanji):
-#    stream = open(KANJI_YAML, 'w')
-
-#def reset_ignore_list():
+def print_kanji(selected):
+    print 'Kanji: '+ selected['kanji']
+    print 'Onyomi: '+ selected['Onyomi']
+    print 'Kunyomi: '+ selected['Kunyomi']
+    print 'Meaning: '+ selected['Meaning']
 
 # Get input from user
 if __name__ == '__main__':
-    get_random_kanji('')
 
-# -l specify level
+    parser = argparse.ArgumentParser()
+    usage = "usage: %prog [options]"
+
+    parser.add_argument("level", type=str,
+                    help="select JLPT level from  N1|N2|...|N5",
+                    default='N5')
+
+    args = parser.parse_args()
+
+    get_random_kanji(args.level)
+
 # -i adds kanji to ignore list
 # Default fetches random kanji
 # optional: specify Kanji
